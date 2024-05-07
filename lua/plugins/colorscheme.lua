2c8107216d6608968a3f93084ba02afcfa18defc
yutytuty <49990984+yutytuty@users.noreply.github.com>
2024-05-07 14:06:15 +0300

    Change to catppuccin-frape

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
 return {
   { "ellisonleao/gruvbox.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "catppuccin-frappe",
     },
   },
 }
